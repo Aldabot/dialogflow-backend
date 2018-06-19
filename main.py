432ff3651f56053d4cbfce4ba18fb1c5fc32895b
bielrv <rosinol.gabriel@gmail.com>
2018-06-19 17:40:32 +0200

    unused functions deleted

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import json
 import re
 import pandas as pd
@@ -194,6 +196,7 @@ def best_lenders(req):
                              'quickReplies': {
                                  'title': 'Ninguna entidad ofrece más de 60.000€ sin un aval hipotecario',
                                  'quickReplies': [
+                                 validNif = True
                                      'Quiero 60.000€',
                                      'Quiero menos de 60.000€',
                                      'Ayúdame'
@@ -223,57 +226,6 @@ def validate_amount_term(req):
             textoError = textoError + 'Lamentablemente no podemos ofrecerte más de 60 días. '
         salida = make_response(jsonify({'fulfillmentText': textoError}))
     return salida
-
-
-def validateDni(req):
-    identificador = req.get('queryResult').get('parameters').get('DNI')
-    nifRegex = '^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$'
-    nieRegex = '^[XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]$'
-    validNif = False
-    validNie = False
-    if re.search(nifRegex, identificador):
-        validNif = True
-
-    if re.search(nieRegex, identificador):
-        validNie = True
-
-    if validNif == False and validNie == False:
-        salida = make_response(jsonify(
-            {'fulfillmentText': 'El formato del identifidador Nif o Nie no es correcto, vuelva a introducirlo.'}))
-        return salida
-
-    return make_response(jsonify(req.get('queryResult').get('fulfillmentMessages')))
-
-
-def validateTlf(req):
-    tlf = req.get('queryResult').get('parameters').get('phone-number')
-    tlfRegex = '^[0-9]{9}$'
-    validTlf = False
-    if re.search(tlfRegex, tlf):
-        validTlf = True
-
-    if validTlf == False:
-        salida = make_response(jsonify({
-            'fulfillmentText': 'El formato del telefono introducido no es correcto, ha de ser 999999999 , vuelva a introducirlo.'}))
-        return salida
-
-    return make_response(jsonify(req.get('queryResult').get('fulfillmentMessages')))
-
-
-def validateEmail(req):
-    email = req.get('queryResult').get('parameters').get('email')
-    emailRegex = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
-    validEmail = False
-    if re.search(emailRegex, email):
-        validEmail = True
-
-    if validEmail == False:
-        salida = make_response(
-            jsonify({'fulfillmentText': 'El formato del email introducido no es correcto, vuelva a introducirlo.'}))
-        return salida
-
-    return make_response(jsonify(req.get('queryResult').get('fulfillmentMessages')))
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8088)
